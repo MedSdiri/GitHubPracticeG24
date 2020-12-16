5fb70a2986b67c5d005a7fbc316a93762472d87c
Ercan Civi <ercancivi@gmail.com>
2020-12-16 17:10:45 -0600

    made a change in Guvanch Test Class

--- GitHubPracticeG24/src/Guvanch/Test.java
+++ GitHubPracticeG24/src/Guvanch/Test.java
@@ -3,6 +3,8 @@ package Guvanch;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello, Hello");
+        System.out.println("How are you?");
 
 
     }
