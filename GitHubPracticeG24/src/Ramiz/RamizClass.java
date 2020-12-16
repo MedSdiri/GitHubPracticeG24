616d54e799f1080d8b339e46e81cac14d87322e7
Ercan Civi <ercancivi@gmail.com>
2020-12-16 17:07:36 -0600

    made a change in Ercan Class and RamizClass

--- GitHubPracticeG24/src/Ramiz/RamizClass.java
+++ GitHubPracticeG24/src/Ramiz/RamizClass.java
@@ -1,4 +1,9 @@
 package Ramiz;
 
 public class RamizClass {
+    public static void main(String[] args) {
+        for(int i=0; i<=100; i++) {
+            System.out.println("Ercan The king");
+        }
+    }
 }
--- GitHubPracticeG24/src/ercan/Ercan.java
+++ GitHubPracticeG24/src/ercan/Ercan.java
@@ -2,6 +2,8 @@ package ercan;
 
 public class Ercan {
     public static void main(String[] args) {
-        System.out.println("Ercan The king");
+        for(int i=0; i<=100; i++) {
+            System.out.println("Ercan The king");
+        }
     }
 }
