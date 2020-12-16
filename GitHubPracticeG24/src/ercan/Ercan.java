e52b467b5c40f08e8a3e227d75b8bd5d307b77c7
Ercan Civi <ercancivi@gmail.com>
2020-12-16 17:44:43 -0600

    added Ladies and Gentlemen

--- GitHubPracticeG24/src/ercan/Ercan.java
+++ GitHubPracticeG24/src/ercan/Ercan.java
@@ -5,6 +5,7 @@ public class Ercan {
         for(int i=0; i<=100; i++) {
             System.out.println("Ercan The king");
             System.out.println("Yes Sir. said Azad");
+            System.out.println("Ladies and Gentlemen");
 
         }
     }
