dcf9534c3445bb19549bc0666dea37664e804b8d
Ercan Civi <ercancivi@gmail.com>
2020-12-16 17:30:26 -0600

    added some change to Test Class

--- GitHubPracticeG24/src/ercan/Test.java
+++ GitHubPracticeG24/src/ercan/Test.java
@@ -3,5 +3,7 @@ package ercan;
 public class Test {
     public static void main(String[] args) {
         //first comment
+        System.out.println("Hi Guys,");
+        System.out.println("How are you today?");
     }
 }
