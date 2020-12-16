bd91dbb64f2adf11930e4b2948ede364fb188dbe
Ercan Civi <ercancivi@gmail.com>
2020-12-16 17:09:40 -0600

    made a change in Azad Practice1 Class and Olesea Task Class

--- GitHubPracticeG24/src/azad/Practice1.java
+++ GitHubPracticeG24/src/azad/Practice1.java
@@ -3,5 +3,7 @@ package azad;
 public class Practice1 {
     public static void main(String[] args) {
 
+        System.out.println("We need more practice");
+
     }
 }
--- GitHubPracticeG24/src/olesea/Task.java
+++ GitHubPracticeG24/src/olesea/Task.java
@@ -4,5 +4,6 @@ public class Task {
     public static void main(String[] args) {
         System.out.println("I forgot the Git/Hub");
         System.out.println("We have to practice more");
+        System.out.println("Me too-Ercan");
     }
 }
