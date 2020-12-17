32b7b94427536ae76c9f15036526d32148bcb614
Ercan Civi <ercancivi@gmail.com>
2020-12-16 20:02:06 -0600

    methods called and print it

--- GitHubPracticeG24/src/ercan/Test.java
+++ GitHubPracticeG24/src/ercan/Test.java
@@ -1,5 +1,12 @@
 package ercan;
 
+import Irina.irina;
+import azad.Azad;
+import guvanch.Test2;
+import olesea.Olesea;
+import ramiz.RamizClass;
+import ahmed.AhmedClass;
+
 public class Test {
     public static void main(String[] args) {
         //first comment
@@ -7,6 +14,23 @@ public class Test {
         System.out.println("How are you today?");
         int a = 0;
         System.out.println(a);
+
+
+
+        Test.methodErcan();
+        System.out.println("============");
+        irina.methodIrina();
+        System.out.println("============");
+        RamizClass.methodRamiz();
+        System.out.println("============");
+        Test2.methodGuvanh();
+        System.out.println("============");
+        Azad.methodAzad();
+        System.out.println("============");
+        Olesea.methodOlesea();
+        System.out.println("============");
+        Test.methodErcan();
+        AhmedClass.methodAhmed();
 
     }
 
