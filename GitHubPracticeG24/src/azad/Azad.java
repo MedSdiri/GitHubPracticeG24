075e1eac0c58efd3e20e476639172334d160740b
AzadBatch21 <sadikhov.azad@gmail.com>
2020-12-16 19:47:22 -0600

    comments 1

--- GitHubPracticeG24/src/azad/Azad.java
+++ GitHubPracticeG24/src/azad/Azad.java
@@ -3,9 +3,10 @@ package azad;
 public class Azad {
     public static void main(String[] args) {
 
+
     }
 
     public static void methodAzad(){
-        System.out.println("Azad is my method");
+        System.out.println("Azad, this is my method");
     }
 }
--- GitHubPracticeG24/src/azad/Test_ByErcan.java
+++ GitHubPracticeG24/src/azad/Test_ByErcan.java
@@ -1,4 +1,27 @@
 package azad;
 
+import Irina.irina;
+import ahmed.AhmedClass;
+import ercan.Test;
+import guvanch.Test2;
+import olesea.Olesea;
+import ramiz.RamizClass;
+
 public class Test_ByErcan {
+    public static void main(String[] args) {
+        Test.methodErcan();
+        System.out.println("============");
+        irina.methodIrina();
+        System.out.println("============");
+        RamizClass.methodRamiz();
+        System.out.println("============");
+        Test2.methodGuvanh();
+        System.out.println("============");
+        Azad.methodAzad();
+        System.out.println("============");
+        Olesea.methodOlesea();
+        System.out.println("============");
+        Test.methodErcan();
+        AhmedClass.methodAhmed();
+    }
 }
