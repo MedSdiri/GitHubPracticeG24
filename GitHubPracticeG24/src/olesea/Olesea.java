575c3e6f52ea8961d9f8eff10f5988fcb826e6b6
Olesea1986 <olesea86williams@gmail.com>
2020-12-16 19:28:53 -0600

    Olesea commit

--- GitHubPracticeG24/src/olesea/Olesea.java
+++ GitHubPracticeG24/src/olesea/Olesea.java
@@ -1,4 +1,10 @@
 package olesea;
 
 public class Olesea {
+    public static void main(String[] args) {
+
+    }
+    public static void methodOlesea() {
+        System.out.println("Olesea this is my feature");
+    }
 }
