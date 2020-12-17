72c17e7874768885a377574be6562608321eaa5e
Irina makaryan <irinamakaryan@yahoo.com>
2020-12-16 20:56:12 -0500

    i did

--- GitHubPracticeG24/src/Irina/irina.java
+++ GitHubPracticeG24/src/Irina/irina.java
@@ -1,5 +1,16 @@
 package Irina;
 
+import ercan.Ercan;
+import ercan.Test;
+import azad.Azad;
+import Irina.irina;
+import olesea.Olesea;
+import ramiz.RamizClass;
+import guvanch.Test2;
+import ahmed.AhmedClass;
+
+
+
 public class irina {
 
    public static void methodIrina(){
@@ -9,6 +20,21 @@ public class irina {
 
     public static void main(String[] args) {
         System.out.println("i hope everything is fine!");
+        Test.methodErcan();
+        System.out.println("============");
+        irina.methodIrina();
+        System.out.println("============");
+        RamizClass.methodRamiz();
+        System.out.println("============");
+        Test2.methodGuvanh();
+        System.out.println("============");
+        Azad.methodAzad();
+        System.out.println("============");
+        Olesea.methodOlesea();
+        System.out.println("============");
+        Test.methodErcan();
+         AhmedClass.methodAhmed();
+
     }
 
-}
+    }
