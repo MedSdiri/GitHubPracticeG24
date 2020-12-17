a3e6a49c347314c3e08392465d6fd559369d9f45
Adil Imanaliev <imanaliev.adil@gmail.com>
2020-12-16 21:25:04 -0500

    My initial Commit

---
+++ GitHubPracticeG24/src/Adil/Practice1.java
@@ -0,0 +1,9 @@
+package Adil;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println("Hello i am trying to figure out my github problem");
+        int a = 0;
+        a = a + 20;
+    }
+}
