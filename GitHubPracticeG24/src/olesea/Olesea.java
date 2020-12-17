16589e3f32b3c89dc90eded255f5e02df2ef59ca
Olesea1986 <olesea86williams@gmail.com>
2020-12-16 19:57:47 -0600

    last

--- GitHubPracticeG24/src/olesea/Olesea.java
+++ GitHubPracticeG24/src/olesea/Olesea.java
@@ -1,10 +1,33 @@
 package olesea;
 
+import Irina.irina;
+import azad.Azad;
+import ercan.Test;
+import guvanch.Test2;
+import ramiz.RamizClass;
+import ahmed.AhmedClass;
+
 public class Olesea {
     public static void main(String[] args) {
 
+
+        Test.methodErcan();
+        System.out.println("============");
+        irina.methodIrina();
+        System.out.println("============");
+        RamizClass.methodRamiz();
+        System.out.println("============");
+        Test2.methodGuvanh();
+        System.out.println("============");
+        Azad.methodAzad();
+        System.out.println("============");
+        Olesea.methodOlesea();
+        System.out.println("============");
+        Test.methodErcan();
+       AhmedClass.methodAhmed();
     }
     public static void methodOlesea() {
         System.out.println("Olesea this is my feature");
+
     }
 }
